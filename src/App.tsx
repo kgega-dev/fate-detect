fcaf94e3bc6410db2c3bb2a633f0d8448bd7f914
KGega <kgega@vub.hr>
2025-12-21 17:47:57 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-[#39de7f]">
+        Test
+      </h1>
+    </div>
+  )
+}
+
+export default App
